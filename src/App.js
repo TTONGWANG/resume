7e30a0f16981d15c55d9cd7205f2576ba1f007b8
wangtapplication@outlook.com <wangtapplication@outlook.com>
2019-11-24 21:14:52 +1100

    can't solve the problem of route

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import Index from './component/Index'
+import About from './component/About'
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        {/* <Index /> */}
+        <Route exact Path="/"> <About /></Route>
+        <Route Path="/Index"> <Index /></Route>
+        <Route Path="/About"> <About /></Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/component/About.js
@@ -0,0 +1,117 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import '../scss/_about.scss'
+import '../scss/main.scss'
+
+class About extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            menuBtn: "menu-btn",
+            hamburger: "menu-btn__burger",
+            nav: "nav",
+            menuNav: "menu-nav",
+            navItems: "menu-nav__item",
+            isToggleOn: false,
+        }
+        this.toggleMenu = this.toggleMenu.bind(this);
+    }
+
+    toggleMenu() {
+        this.setState({
+            isToggleOn: !this.state.isToggleOn,
+            hamburger: this.state.isToggleOn ? 'menu-btn__burger' : 'menu-btn__burger open',
+            nav: this.state.isToggleOn ? 'nav' : 'nav open',
+            menuNav: this.state.isToggleOn ? 'menu-nav' : 'menu-nav open',
+            navItems: this.state.isToggleOn ? 'menu-nav__item' : 'menu-nav__item open',
+        })
+    }
+
+    render() {
+        const {hamburger, nav, menuNav, navItems } = this.state;
+        // console.log(hamburger)
+        console.log(nav)
+        console.log(menuNav)
+
+        return (
+            <body>
+                <header>
+                    <div onClick={this.toggleMenu} ref="myfirstDiv" className="menu-btn">
+                        <span ref="mysecondDiv" className={hamburger}></span>
+                    </div>
+
+                    <nav ref="mythreeDiv" className={nav}>
+                        <ul ref="myfourDiv" className={menuNav}>
+                            <li ref="myfiveDiv" className={navItems}>
+                                <a href="Index.js" className="menu-nav__link" >
+                                    Home
+                            </a>
+                            </li>
+                            <li ref="myfiveDiv" className={navItems}>
+                                <a href='About.js' className="menu-nav__link" >
+                                    About Me
+                            </a>
+                            </li>
+                            <li ref="myfiveDiv" className={navItems}>
+                                <a href="Projects.html" className="menu-nav__link" >
+                                    My Projects
+                            </a>
+                            </li>
+                            <li ref="myfiveDiv" className={navItems}>
+                                <a href="Contact.html" className="menu-nav__link" >
+                                    Contact Me
+                            </a>
+                            </li>
+                        </ul>
+                    </nav>
+                </header>
+
+                <main>
+                    <section className="about">
+                        <div className="about__bio-image">
+                            <div className="about__bio">
+                                <h2 className="text-secondary">BIO</h2>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae culpa sapiente rerum dolores aliquam, fugiat eveniet commodi. Vel, nihil, numquam possimus deserunt officiis rerum in omnis error esse iusto quas.</p>
+                            </div>
+                        </div>
+
+                        <div className="jobs">
+                            <div className="jobs__job">
+                                <h2 className="text-secondary">2017 - Current</h2>
+                                <h3>Google</h3>
+                                <h6>Full Stack Developer</h6>
+                                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat vero dolores eum ut, culpa ullam quis quasi exercitationem in adipisci.</p>
+                            </div>
+                            <div className="jobs__job">
+                                <h2 className="text-secondary">2015 - 2017</h2>
+                                <h3>Microsoft</h3>
+                                <h6>Front End Developer</h6>
+                                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat vero dolores eum ut, culpa ullam quis quasi exercitationem in adipisci.</p>
+                            </div>
+                            <div className="jobs__job">
+                                <h2 className="text-secondary">2012 - 2015</h2>
+                                <h3>Web Design Co.</h3>
+                                <h6>Graphic Designer</h6>
+                                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat vero dolores eum ut, culpa ullam quis quasi exercitationem in adipisci.</p>
+                            </div>
+                        </div>
+
+                        <div className="social-icons">
+                            <a href="#!"><i className="github"><FontAwesomeIcon icon={faCoffee} /></i> </a>
+                            <a href="#!"><i className="linkedIn"><FontAwesomeIcon icon={faCoffee} /></i></a>
+                        </div>
+                        <footer> &copy; Copyright 2019 </footer>
+                    </section>
+                </main>
+
+            </body>
+        )
+    }
+}
+
+
+export default About;
+
+
+
